5c61a97e5df82f305a55b2432a7c4c417ec8d75f
oz001 <7475621+oz001@users.noreply.github.com>
2021-08-18 14:20:48 +0900

    add stateEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage.jsx";
 
 const App = () => {
   console.log("最初");
-  const [num, setNum] = useState(1000);
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
 
   const onClickCountUp = () => {
     setNum(num + 1);
+  };
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        console.log("in if " + num);
+        console.log(faceShowFlag);
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        console.log("in else " + num);
+        console.log(faceShowFlag);
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num, faceShowFlag]);
+
+  const onClickFace = () => {
+    setFaceShowFlag(!faceShowFlag);
   };
 
   return (
@@ -16,7 +34,8 @@ const App = () => {
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
       <button onClick={onClickCountUp}>ボタン</button>
       <p>{num}</p>
-      <button>On/OFF</button>
+      <button onClick={onClickFace}>On/OFF</button>
+      {faceShowFlag && <p>(oo)/</p>}
     </>
   );
 };
