f0ebe47f7d30b81b5551b17b8df7f493be08e119
oz001 <7475621+oz001@users.noreply.github.com>
2021-08-18 13:03:14 +0900

    修正

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,21 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage.jsx";
 
 const App = () => {
-  const onClickButton = () => {
+  console.log("最初");
+  const [num, setNum] = useState(1000);
+
+  const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
 
-  const contentLadyStyle = {
-    color: "pink",
-    fontSize: "18px"
-  };
   return (
     <>
       <h1 style={{ color: "red" }}>hello!</h1>
       <ColorfulMessage color="blue">元気ですか＝＝</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
-      {num}
+      <button onClick={onClickCountUp}>ボタン</button>
+      <p>{num}</p>
+      <button>On/OFF</button>
     </>
   );
 };
